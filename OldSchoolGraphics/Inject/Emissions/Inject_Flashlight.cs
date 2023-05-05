fa9ff5b3cb4bd77cc8081ec47b6a955b96172fa9
Flowaria <wnsghgj1234@naver.com>
2023-05-06 04:13:06 +0900

    Added Flashlight Adjustment

---
+++ OldSchoolGraphics/Inject/Emissions/Inject_Flashlight.cs
@@ -0,0 +1,32 @@
+﻿using GameData;
+using Gear;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldSchoolGraphics.Inject.Emissions;
+[HarmonyPatch(typeof(PlayerInventoryBase))]
+internal class Inject_Flashlight
+{
+    static float Mult => (1.15f / Math.Max(0.2f, CFG.ExposureScale.Value)) * 2.1f;
+
+    [HarmonyPatch(nameof(PlayerInventoryBase.PrepareHelmetFlashlight))]
+    [HarmonyPostfix]
+    static void Post_HelmetLight(PlayerInventoryBase __instance)
+    {
+        __instance.m_flashlight.intensity *= Mult;
+    }
+
+    [HarmonyPatch(nameof(PlayerInventoryBase.OnItemEquippableFlashlightWielded))]
+    [HarmonyPostfix]
+    static void Post_GearLight(PlayerInventoryBase __instance, GearPartFlashlight flashlight)
+    {
+        if (flashlight.m_settingsID > 0)
+        {
+            var data = FlashlightSettingsDataBlock.GetBlock(flashlight.m_settingsID);
+            __instance.m_flashlight.intensity = data.intensity * Mult;
+        }
+    }
+}
